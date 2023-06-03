f6a73a22b893bec7ecc6e27e3e0d2962c43a16c6
0reilly <oreilly.119@osu.edu>
2023-06-02 20:35:55 -0400

    updated home page and trial page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,119 +1,88 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const LandingPage = () => {
+const Home: React.FC = () => {
+    const Title: React.FC = () => (
+        <h1 className="text-3xl font-bold text-gray-900 text-center">
+            RocketStats: Your Privacy-Friendly Analytics Alternative
+        </h1>
+    );
+
+    const Subtitle: React.FC = () => (
+        <h2 className="text-xl text-gray-600 text-center">
+            Website analytics doesn't have to compromise privacy. With RocketStats, get meaningful insights while respecting user data.
+        </h2>
+    );
+
+    const CallToAction: React.FC = () => (
+        <div className="mt-5 flex justify-center">
+            <a
+                href="/trial"
+                className="inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+            >
+                Get started for free
+            </a>
+        </div>
+    );
+
+    const Features: React.FC = () => (
+        <ul className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <li className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                <p className="text-lg leading-6 font-medium text-gray-900 text-center">
+                    No cookie notices required
+                </p>
+            </li>
+            <li className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                <p className="text-lg leading-6 font-medium text-gray-900 text-center">
+                    GDPR Compliant
+                </p>
+            </li>
+            <li className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                <p className="text-lg leading-6 font-medium text-gray-900 text-center">
+                    30-day free trial
+                </p>
+            </li>
+        </ul>
+    );
+
+
+    const UserGuide: React.FC = () => (
+        <section className="mt-10">
+            <h2 className="text-2xl font-bold text-gray-900">
+                Getting Started With RocketStats is Easy
+            </h2>
+            <div className="mt-4 text-gray-600">
+                <ol className="list-decimal list-inside">
+                    <li>Sign up for an account. Start with a 30-day free trial, then pricing starts at $14/month.</li>
+                    <li>Add our script. It's a single line of code that works with every site, web app, and CMS.</li>
+                    <li>See your stats. Your RocketStats dashboard will start showing you real-time data.</li>
+                </ol>
+            </div>
+        </section>
+    );
+
+    const AboutUs: React.FC = () => (
+        <section className="mt-10">
+            <h2 className="text-2xl font-bold text-gray-900">
+                RocketStats: Putting User Privacy First
+            </h2>
+            <div className="mt-4 text-gray-600">
+                <p>
+                    We built RocketStats to be privacy-focused and easy to use. Our business model is straightforward and unusual for the industry: we charge a fair price for our software instead of giving it away for free, then selling your data to generate revenue. We believe that by supporting RocketStats, you are taking a step towards a more ethical, privacy-respecting approach to website analytics.
+                </p>
+            </div>
+        </section>
+    );
+
     return (
-        <div className="min-h-screen bg-white">
-            <nav className="bg-blue-600">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-center justify-between h-16">
-                        <div className="flex items-center">
-                            <div className="flex-shrink-0">
-                                <img className="h-8 w-8" src="/logo.svg" alt="RocketStats Logo" />
-                            </div>
-                            <div className="hidden md:block">
-                                <div className="ml-10 flex items-baseline space-x-4">
-                                    <Link className="px-3 py-2 rounded-md text-sm font-medium text-white bg-blue-800" to=''>Home</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <header className="bg-blue-700 py-10">
-                <div className="text-center">
-                    <h1 className="text-6xl font-bold text-white">RocketStats</h1>
-                    <p className="mt-2 text-lg text-blue-200">Your alternative to Google Analytics</p>
-                    <div className="mt-8">
-                        <Link to="/trial" className="inline-block px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-blue-700 bg-white hover:bg-blue-100">Start Your Trial</Link>
-                    </div>
-                </div>
-            </header>
-
-            <main>
-                <div className="bg-gray-100 py-10">
-                    <h2 className="text-2xl font-bold text-center text-gray-700">Why RocketStats?</h2>
-                    <div className="mt-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <p className="text-center text-gray-500">
-                            RocketStats offers a fresh approach to website analytics. We prioritize privacy, simplicity, and effective insights.
-                        </p>
-
-                        {/* Features Section */}
-                        <section className="pt-10">
-                            <div className="mt-10">
-                                <h3 className="text-lg leading-6 font-medium text-gray-900">Key Features</h3>
-                                <div className="mt-4 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-                                    <div>
-                                        <h4 className="text-lg leading-6 font-medium text-gray-900">User Privacy</h4>
-                                        <p className="mt-2 text-base text-gray-500">
-                                            We respect your users' privacy and don't use cookies to track their visits.
-                                        </p>
-                                    </div>
-
-                                    <div>
-                                        <h4 className="text-lg leading-6 font-medium text-gray-900">Real-time Data</h4>
-                                        <p className="mt-2 text-base text-gray-500">
-                                            Get real-time insights into your website's traffic patterns and visitor behavior.
-                                        </p>
-                                    </div>
-
-                                    <div>
-                                        <h4 className="text-lg leading-6 font-medium text-gray-900">Easy Integration</h4>
-                                        <p className="mt-2 text-base text-gray-500">
-                                            Integrating RocketStats into your website is as simple as copying and pasting a line of code.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-
-                        {/* Customer Review Section */}
-                        <section className="pt-10">
-                            <h3 className="text-lg leading-6 font-medium text-gray-900">What our customers say</h3>
-                            <div className="mt-8">
-                                <blockquote>
-                                    <div>
-                                        <p className="text-base text-gray-500">
-                                            "RocketStats gives me the insights I need without compromising the privacy of my users. It's an essential tool for my website."
-                                        </p>
-                                    </div>
-                                    <footer className="mt-3">
-                                        <div className="flex items-center space-x-3">
-                                            <div className="flex-shrink-0">
-                                                <img className="h-6 w-6 rounded-full" src="/avatar.jpg" alt=""/>
-                                            </div>
-                                            <div className="text-base font-medium text-gray-700">
-                                                Jessica, website owner
-                                            </div>
-                                        </div>
-                                    </footer>
-                                </blockquote>
-                            </div>
-                        </section>
-
-                        {/* Call to Action Section */}
-                        <section className="py-10 text-center">
-                            <h3 className="text-2xl font-semibold text-gray-700">Start Your Free Trial Now</h3>
-                            <p className="mt-4 text-lg text-gray-500">Get started with RocketStats today and see the difference for yourself.</p>
-                            <div className="mt-8">
-                                <Link to="/trial" className="inline-block px-5 py-3 border border-transparent shadow text-base font-medium rounded-md text-blue-700 bg-white hover:bg-blue-100">Start Your Trial</Link>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </main>
-
-
-            <footer className="bg-blue-700 py-5">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <p className="text-center text-base text-blue-200">
-                        &copy; {new Date().getFullYear()} RocketStats. All rights reserved.
-                    </p>
-                </div>
-            </footer>
+        <div className="p-10">
+            <Title />
+            <Subtitle />
+            <CallToAction />
+            <Features />
+            <UserGuide />
+            <AboutUs />
         </div>
     );
 };
 
-export default LandingPage;
+export default Home;
--- src/Trial.tsx
+++ src/Trial.tsx
@@ -1,14 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const TrialPage = () => {
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, formData: { email: string; password: string; plan: string; }) => {
+    event.preventDefault();
+    console.log(formData);
+};
+
+const TrialPage: React.FC = () => {
+    const [ formData, setFormData ] = useState({
+        email: '',
+        password: '',
+        plan: '',
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setFormData({
+            ...formData,
+            [ event.target.name ]: event.target.value,
+        });
+    };
+
     return (
-        <div className="bg-gray-100 h-screen">
-            <header className="text-center py-16">
-                <h1 className="text-6xl font-bold text-blue-700">Start Your Trial</h1>
-                <p className="text-2xl text-blue-500 mt-4">Welcome to the RocketStats trial sign up</p>
-            </header>
+        <div className="p-10 flex flex-col items-center">
+            <h1 className="text-3xl font-bold text-gray-900 text-center">Start Your Trial</h1>
 
-            // You can add a form or more details here...
+            <form
+                onSubmit={ (event) => handleSubmit(event, formData) }
+                className="flex flex-col items-center w-full max-w-md mt-5"
+            >
+                <label className="w-full mb-4">
+                    Your email address:
+                    <input
+                        type="email"
+                        name="email"
+                        value={ formData.email }
+                        onChange={ handleChange }
+                        required
+                        className="mt-2 w-full p-2 border rounded"
+                    />
+                </label>
+
+                <label className="w-full mb-4">
+                    Create a password:
+                    <input
+                        type="password"
+                        name="password"
+                        value={ formData.password }
+                        onChange={ handleChange }
+                        required
+                        className="mt-2 w-full p-2 border rounded"
+                    />
+                </label>
+
+                <label className="w-full mb-4">
+                    Choose a plan:
+                    <select
+                        name="plan"
+                        value={ formData.plan }
+                        onChange={ handleChange }
+                        required
+                        className="mt-2 w-full p-2 border rounded"
+                    >
+                        <option value="">--Please choose a plan--</option>
+                        <option value="free">Free Trial</option>
+                        <option value="starter">$14/m (Up to 100,000 monthly page views)</option>
+                        <option value="professional">$24/m (Up to 200,000 monthly page views)</option>
+                        <option value="enterprise">$44/m (Up to 500,000 monthly page views)</option>
+                    </select>
+                </label>
+
+                <button
+                    type="submit"
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+                >
+                    Start Your Trial
+                </button>
+            </form>
+
+            <Link
+                to="/"
+                className="mt-5 text-indigo-600 hover:text-indigo-500"
+            >
+                Back to homepage
+            </Link>
         </div>
     );
 };
